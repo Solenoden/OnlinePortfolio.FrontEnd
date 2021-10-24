31048517c111a74a7bc72eb057bc6767a1e91b5d
Gavin de Bruyn <gavindb@runninghill.co.za>
2021-10-24 09:23:20 +0200

    Create About Me page.
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

---
+++ src/app/components/about-me-page/about-me-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { AboutMePageComponent } from './about-me-page.component'
+
+describe('AboutMePageComponent', () => {
+    let component: AboutMePageComponent
+    let fixture: ComponentFixture<AboutMePageComponent>
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ AboutMePageComponent ]
+        }).compileComponents()
+    })
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AboutMePageComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
