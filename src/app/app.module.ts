21e6ac4b2b10759ad5f72a8b629bd1fbbc5d00c5
Gavin de Bruyn <gavindb@runninghill.co.za>
2021-10-24 09:26:23 +0200

    Fix linting issue.
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component'
 import { LandingPageComponent } from './components/landing-page/landing-page.component'
 import { NavigationBarComponent } from './components/common/navigation-bar/navigation-bar.component'
 import { HomePageComponent } from './components/home-page/home-page.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AboutMePageComponent } from './components/about-me-page/about-me-page.component'
 
 @NgModule({
